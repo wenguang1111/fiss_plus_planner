54d793a8b05af78dbb60798114ea8631cea3e50b
Kareem Mohamed <kareemmagdy420@gmail.com>
2025-12-20 23:57:21 +0100

    use poetry as dependency manager and resolve imports of planners module

---
+++ fiss_plus_planner/planners/fop_plus_planner.py
@@ -0,0 +1,42 @@
+from queue import PriorityQueue
+
+from commonroad.scenario.obstacle import Obstacle
+from commonroad.scenario.scenario import Scenario
+from commonroad.scenario.state import InitialState
+
+from fiss_plus_planner.planners.common.scenario.frenet import FrenetState, FrenetTrajectory
+from fiss_plus_planner.planners.common.vehicle.vehicle import Vehicle
+from fiss_plus_planner.planners.frenet_optimal_planner import FrenetOptimalPlanner, FrenetOptimalPlannerSettings, Stats
+
+
+class FopPlusPlanner(FrenetOptimalPlanner):
+    def __init__(self, planner_settings: FrenetOptimalPlannerSettings, ego_vehicle: Vehicle):
+        super().__init__(planner_settings, ego_vehicle)
+        self.candidate_trajs = PriorityQueue()
+
+    def plan(self, frenet_state: FrenetState, max_target_speed: float, obstacles: list[Obstacle], time_step_now: int = 0, initial_state: InitialState = None) -> FrenetTrajectory:
+        # reset stats
+        self.stats = Stats()
+        self.settings.highest_speed = max_target_speed
+        
+        fplist = self.calc_frenet_paths(frenet_state)
+        fplist = self.calc_global_paths(fplist)
+        self.stats.num_trajs_generated = len(fplist)
+        
+        self.candidate_trajs = PriorityQueue()
+        for traj in fplist:
+            self.candidate_trajs.put(traj)
+
+        while not self.candidate_trajs.empty():
+            self.stats.num_iter += 1
+            candidate = self.candidate_trajs.get()
+            passed_candidate = self.check_constraints([candidate])
+            self.stats.num_trajs_validated += 1
+            safe_candidate = self.check_collisions(passed_candidate, obstacles, time_step_now)
+            self.stats.num_collison_checks += 1
+            # safe_candidate = self.check_collisions(passed_candidate, time_step_now)
+            if safe_candidate:
+                self.best_traj = safe_candidate[0]
+                return self.best_traj
+
+        return None
