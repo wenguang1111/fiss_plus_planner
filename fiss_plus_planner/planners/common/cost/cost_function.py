14d69948ddce9ae496ac2c779525a6f4ad2da22e
wenguang <xuwenguang1111@gmail.com>
2026-02-27 00:22:30 +0100

    update cost

--- fiss_plus_planner/planners/common/cost/cost_function.py
+++ fiss_plus_planner/planners/common/cost/cost_function.py
@@ -10,7 +10,7 @@ class CostFunction:
             self.w_J = 1.0
             self.w_D = 10.0
             self.w_LC = 10.0
-            self.w_dist = 0.1
+            self.w_dist = 0.01
             self.max_speed = 14.0  # for normalization, can be adjusted based on
 
     
